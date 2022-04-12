46fe71f7d593b4eaf86ab602cb90aeb35223b451
EstherZC <estherzc99@gmail.com>
2022-04-12 16:08:07 +0100

    Api rest with JWT (v1.0.1) update token response

--- apirest/src/main/java/com/bitboxer2prueba/estherzurita/apirest/controller/TokenController.java
+++ apirest/src/main/java/com/bitboxer2prueba/estherzurita/apirest/controller/TokenController.java
@@ -10,12 +10,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/authenticated")
 public class TokenController {
 
@@ -31,6 +29,6 @@ public class TokenController {
                 .authenticate(new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = tokenProvider.generateToken(authentication);
-        return ResponseEntity.ok(new JwtResponse(jwt));
+        return ResponseEntity.ok(new JwtResponse(jwt, 200));
     }
 }
--- apirest/src/main/java/com/bitboxer2prueba/estherzurita/apirest/model/JwtResponse.java
+++ apirest/src/main/java/com/bitboxer2prueba/estherzurita/apirest/model/JwtResponse.java
@@ -2,13 +2,20 @@ package com.bitboxer2prueba.estherzurita.apirest.model;
 
 public class JwtResponse {
 
+    private final Integer status;
     private final String token;
 
-    public JwtResponse(String token) {
+    public JwtResponse(String token, Integer status)
+    {
+        this.status = status;
         this.token = token;
     }
 
     public String getToken() {
         return token;
+    }
+
+    public Integer getStatus(){
+        return status;
     }
 }
